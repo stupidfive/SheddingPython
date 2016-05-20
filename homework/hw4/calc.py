4c1bd349f628f0fde1e5b40b716ba408c4d3164f
GeorgeWu <wuzheng@zerozero.cn>
2016-05-20 19:17:49 +0800

    Day 4.

---
+++ homework/hw4/calc.py
@@ -0,0 +1,14 @@
+# Fill out the formula. First one already been given.
+a = 5 + 3
+b
+c
+d
+e
+f
+
+print '5 + 3 = '
+print '5 - 3 = '
+print '5 * 3 = '
+print '5 / 3 = '
+print '5 mod 3 = '
+print '5 to the power of 3 = '
---
+++ notes/day4/day4.md
@@ -0,0 +1,5 @@
+# Day 4 - Basic Arithmatics
+
+Doing arithmatic in python is just like any other scientific calculator. For instance
+
+    print 306 + 214 # 520
