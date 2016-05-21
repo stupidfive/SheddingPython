120b3ea28796e3351b6d78884991061ec3dab45b
GeorgeWu <wuzheng@zerozero.cn>
2016-05-21 18:48:51 +0800

    Day 5.

---
+++ homework/hw5/listing.py
@@ -0,0 +1,20 @@
+lst = ['a', 'b', 'c', 'd', 'e']
+print 'Originally, the list is: %s' % lst
+
+# Your solution here.
+print 'The third element in the list is: '
+
+# Now change the third element to 'i'
+
+print "Changed the third element to 'i', now the list is: %s" % lst
+
+# Remove the first element from list
+
+print "Removed the first element, now the list is: %s" % lst
+
+# Add a 't' at the end of list
+
+print "Added a 't' at the end, now the list is: %s" % lst
+
+# Fill your solution at the end of line.
+print 'At last the word is: %s, which has %d letters.'
---
+++ notes/day5/day5.md
@@ -0,0 +1,3 @@
+Day 5 - Listing
+
+Sometime you may want to deal with an array of things. Python has just the thing for you - list.
