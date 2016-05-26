609acdcaebc97ff27520f1c42b0511af2c1e0416
GeorgeWu <wuzheng@zerozero.cn>
2016-05-26 23:02:45 +0800

    Day 8.

---
+++ homework/hw8/gcd.py
@@ -0,0 +1,10 @@
+import random
+
+a = random.randint(10 ** 9, 10 ** 10)
+b = random.randint(10 ** 9, 10 ** 10)
+
+# Your code here
+
+
+
+print 'gcd(%d, %d) = %d' % (a, b, gcd)
---
+++ notes/day8/day8.md
@@ -0,0 +1,7 @@
+# Day 8
+
+More practise on handling control flow and basic arithematics.
+
+## Homework
+
+Finish `gcd.py`
