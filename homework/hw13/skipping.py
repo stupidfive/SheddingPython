3df6a03d451667b5ce42650a09f5c1b324bb45a4
GeorgeWu <wuzheng@zerozero.cn>
2016-06-11 02:29:50 +0800

    Day 13.

---
+++ homework/hw13/skipping.py
@@ -0,0 +1,12 @@
+# Print every non-negative integer under 1000 that's not a square of 
+# another integer. For instance, 4 is 2 squared, so 4 shall not 
+# printed.
+# Try do this with `continue` statement.
+
+for x in range(1000):
+    # Your code starts here.
+
+
+    # Your code ends here. 
+
+    print x
---
+++ notes/day13/day13.md
@@ -0,0 +1,3 @@
+Day 13 - Skip
+
+Like I said the day before, looping is time consuming. `break` helps end the loop early. But what if we want to skip the current round while go on looping the rest? That's where `continue` comes into play.
